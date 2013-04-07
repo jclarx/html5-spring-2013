872828453ef14a1ac0ad06777355ab2117f8e164
Joan Clark <jclarx@comast.net>
2013-04-06 18:08:35 -0700

    Edge Animation files
    This commit includes all the newer animation files for the Blair and
    Jerry's web animation

---
+++ Edge Animations/plane-clouds-suitcase-FinalDraft1/plane-cloud-seq7b_edgeActions.js
@@ -0,0 +1,12 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'goldClouds2'
+(function(symbolName){})("goldClouds2");
+//Edge symbol end:'goldClouds2'
+})(jQuery,AdobeEdge,"EDGE-88638607");
