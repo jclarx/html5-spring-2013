8ae8f41e1928bbc1c17291e613b80af50089dd6d
Joan Clark <jclarx@comast.net>
2013-04-12 18:09:17 -0700

    moved files, added latest edge animation

---
+++ Edge Animations/test010/plane-cloud-seq10_edgeActions.js
@@ -0,0 +1,35 @@
+/***********************
+* Adobe Edge Animate Composition Actions
+*
+* Edit this file with caution, being careful to preserve 
+* function signatures and comments starting with 'Edge' to maintain the 
+* ability to interact with these actions from within Adobe Edge Animate
+*
+***********************/
+(function($, Edge, compId){
+var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonly used Edge classes
+
+   //Edge symbol: 'stage'
+   (function(symbolName) {
+      
+      
+   })("stage");
+   //Edge symbol end:'stage'
+
+   //=========================================================
+   
+   //Edge symbol: 'goldClouds2'
+   (function(symbolName) {   
+   
+   })("goldClouds2");
+   //Edge symbol end:'goldClouds2'
+
+   //=========================================================
+   
+   //Edge symbol: 'Down-level'
+   (function(symbolName) {   
+   
+   })("Down-level");
+   //Edge symbol end:'Down-level'
+
+})(jQuery, AdobeEdge, "EDGE-88638607");
