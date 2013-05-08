735d063a313e0330f6b30ae6d7cd91a0df7cb148
Joan Clark <jclarx@comast.net>
2013-05-07 21:27:07 -0700

    Almost-final Animation
    Blair and Jerry's Wine Shop animation. These files are ready to go! I
    expect one more update after this sometime this week.

---
+++ final-project-animation/ani5/wineshop_edgeActions.js
@@ -0,0 +1,28 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindSymbolAction(compId,symbolName,"creationComplete",function(sym,e){yepnope({load:"http://cdn.edgecommons.org/an/1.0.0/js/min/EdgeCommons.js",complete:function(){}});});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Down-level'
+(function(symbolName){})("Down-level");
+//Edge symbol end:'Down-level'
+
+//=========================================================
+
+//=========================================================
+
+//Edge symbol: 'Preloader'
+(function(symbolName){})("Preloader");
+//Edge symbol end:'Preloader'
+
+//=========================================================
+
+//Edge symbol: 'goldClouds2'
+(function(symbolName){})("goldClouds2");
+//Edge symbol end:'goldClouds2'
+})(jQuery,AdobeEdge,"EDGE-88638607");
